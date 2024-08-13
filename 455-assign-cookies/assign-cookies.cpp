7fb6b1c0070b00cb01d771a17ae54f6c0b3f04a8
Sumit Prakash <sumit.prakash@students.iiit.ac.in>
2024-08-13 22:09:59 +0530

    Time: 24 ms (24.61%) | Memory: 20.9 MB (43.79%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int solve(int i, int j, int n, int m,vector<int>& g, vector<int>& s){
-        if(i==n||j==m) return 0;
+    // int solve(int i, int j, int n, int m,vector<int>& g, vector<int>& s){
+    //     if(i==n||j==m) return 0;
    
-        int give=0;
-        if(s[j]>=g[i]){
-            give=1+solve(i+1,j+1,n,m,g,s);
-        }else return solve(i,j+1,n,m,g,s);
-        return give;
-    }
+    //     int give=0;
+    //     if(s[j]>=g[i]){
+    //         give=1+solve(i+1,j+1,n,m,g,s);
+    //     }else return solve(i,j+1,n,m,g,s);
+    //     return give;
+    // }
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int n=g.size();
         int m=s.size();
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        return solve(0,0,n,m,g,s);
+        int l=0,r=0;
+        while(l<m&&r<n){
+            if(g[r]<=s[l]) r++;
+            l++;
+        }
+        return r;
         
     }
 };
