f01f2fc237c8e8de42cdeb3d6d9fa9bf8ea0e6de
Sumit Prakash <sumit.prakash@students.iiit.ac.in>
2024-09-06 11:14:31 +0530

    Time: 11 ms (57.13%) | Memory: 13.9 MB (58.11%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int solve(int ind,int target, vector<int> & nums){
+    int solve(int ind,int target, vector<int> & nums,vector<vector<int>>& dp){
         if(ind==0){
-            if(target==0&&nums[ind]==0) return 2;
-            if(target-nums[ind]==0||target+nums[ind]==0) return 1;
+            if(target==0&&nums[0]==0) return 2;
+            if(target==0||nums[0]==target) return 1;
             else return 0;
         }
-        int plus=solve(ind-1,target-nums[ind],nums);
-        int minus=solve(ind-1,target+nums[ind],nums);
-        return plus+minus;
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int minus=solve(ind-1,target,nums,dp);
+        int plus=0;
+        if(nums[ind]<=target)  plus=solve(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return solve(n-1,target,nums);
+        int tot=0;
+        for(int i=0;i<n;i++) tot+=nums[i];
+        if(tot-target<0) return 0;
+        if((tot-target)%2==1) return 0;
+        int sum2=(tot-target)/2;
+        vector<vector<int>> dp(n,vector<int> (sum2+1,-1));
+        return solve(n-1,sum2,nums,dp);
         
     }
 };
