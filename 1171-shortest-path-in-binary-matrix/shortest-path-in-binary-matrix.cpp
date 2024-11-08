9de29f52cfa372b2feaa01524464d1ea3b35b739
Sumit Prakash <sumit.prakash@students.iiit.ac.in>
2024-11-08 21:51:20 +0530

    Time: 20 ms (59.01%) | Memory: 25.3 MB (38.81%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,28 +3,23 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         if(grid[0][0]==1) return -1;
-        vector<vector<int>> vis(n,vector<int> (n,0));
         vector<vector<int>> dist(n,vector<int> (n,1e9));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({1,{0,0}});
-        vis[0][0]=1;
+        queue<pair<int,pair<int,int>>> q;
+        q.push({1,{0,0}});
         dist[0][0]=1;
         int dx[]={0,0,1,-1,-1,1,-1,1};
         int dy[]={1,-1,0,0,-1,1,1,-1};
-        while(!pq.empty()){
-            int dis=pq.top().first;
-            int x=pq.top().second.first;
-            int y=pq.top().second.second;
-            pq.pop();
+        while(!q.empty()){
+            int dis=q.front().first;
+            int x=q.front().second.first;
+            int y=q.front().second.second;
+            q.pop();
             for(int i=0;i<8;i++){
                 int nx=x+dx[i];
                 int ny=y+dy[i];
-                if(nx>=0&&ny>=0&&nx<n&&ny<n&&grid[nx][ny]==0&&vis[nx][ny]==0){
-                    vis[nx][ny]=1;
-                    if(dist[nx][ny]>dis+1){
-                        dist[nx][ny]=dis+1;
-                        pq.push({dis+1,{nx,ny}});
-                    }
+                if(nx>=0&&ny>=0&&nx<n&&ny<n&&grid[nx][ny]==0&&dist[nx][ny]>dis+1){                    
+                    dist[nx][ny]=dis+1;
+                    q.push({dis+1,{nx,ny}});                    
                 }
             }
             
