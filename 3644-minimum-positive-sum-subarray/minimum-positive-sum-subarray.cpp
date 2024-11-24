5a45e7928a92d84a6612430d6fc00ea9a274d12c
Sumit Prakash <sumit.prakash@students.iiit.ac.in>
2024-11-24 18:08:58 +0530

    Time: 1750 ms (8.33%) | Memory: 29.6 MB (8.33%) - LeetSync

---
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n=nums.size();
+        set<int> st;
+        for(int i=l;i<=r;i++) st.insert(i);
+        int mini=INT_MAX;
+        int sum;
+        for(int i=0;i<n;i++){
+            sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                int len=j-i+1;
+                if(st.find(len)!=st.end()&&sum>0){
+                    mini=min(mini,sum);
+                }
+                cout<<sum<<" ";
+            }
+        }
+        if(mini==0||mini==INT_MAX) return -1;
+        return mini;        
+    }
+};
